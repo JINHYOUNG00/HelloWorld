c538caed6af04baaaace176f4e62d7e125ec45e4
admin <admin@YD2-05>
2022-04-11 14:23:02 +0900

    com/pu

--- HelloWorld/src/co/edu/variable/ifExample.java
+++ HelloWorld/src/co/edu/variable/ifExample.java
@@ -7,7 +7,7 @@ public class ifExample {
 		boolean over20; // 변수의 길이는 상관x 의미있는 변수명 사용. 변수의 첫글자는 대문자, 숫자 x
 		over20 = myAge > 20; //over20 == myAge > 20
 		
-		if(over20); {
+		if(over20) {
 			System.out.println("over 20");
 		} else {
 			System.out.println("under 20");
