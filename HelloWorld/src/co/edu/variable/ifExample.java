974136b1cb0c87f09a381c1cdf764c0d1287f07e
admin <admin@YD2-05>
2022-04-11 14:09:59 +0900

    비교문

---
+++ HelloWorld/src/co/edu/variable/ifExample.java
@@ -0,0 +1,18 @@
+package co.edu.variable;
+
+public class ifExample {
+
+	public static void main(String[] args) {
+		int myAge = 19;
+		boolean over20; // 변수의 길이는 상관x 의미있는 변수명 사용. 변수의 첫글자는 대문자, 숫자 x
+		over20 = myAge > 20; //over20 == myAge > 20
+		
+		if(over20); {
+			System.out.println("over 20");
+		} else {
+			System.out.println("under 20");
+		}
+		
+	}
+
+}
