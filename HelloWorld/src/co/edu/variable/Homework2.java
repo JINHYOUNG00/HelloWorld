9f1fc213f02c3848ca5eb9c3b07fae8827167036
admin <admin@YD2-05>
2022-04-11 17:46:57 +0900

    마지막

--- HelloWorld/src/co/edu/variable/Homework2.java
+++ HelloWorld/src/co/edu/variable/Homework2.java
@@ -7,6 +7,8 @@ public class Homework2 {
 	public static void main(String[] args) {
 
 		Scanner scn = new Scanner(System.in);
+		System.out.println("보유현금을 입력하시오.");
+		int money = scn.nextInt();
 		System.out.println("물건의 가격을 입력하시오.");
 		int price = scn.nextInt();
 	}
@@ -17,5 +19,5 @@ public class Homework2 {
 //물건의 가격과 현금.
 //[현금] 10000
 //[물건가격] 7500 (부가세 포함가격)
-//[부가세] 682 (부가세는 물건가격 1/10) (물건가격 6818)
+//[부가세] 682 (부가세는 물건가격 1/10)(합계금액 / 1.1 = 공급가액) (물건가격 6818)
 //[잔돈] 2500
